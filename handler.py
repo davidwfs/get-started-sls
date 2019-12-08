f8db01d79961c33e7878b4103e5aa7deaea23ea3
David Washington <davidwashington833@gmail.com>
2019-12-08 04:19:56 -0300

    Create function getValueById

--- handler.py
+++ handler.py
@@ -1,7 +1,16 @@
 import json
 
+values = ["v1", "v2", "v3"]
+
+def getValueById(event, context):
+    id = int(event["pathParameters"]["id"])
+    return {
+        "statusCode": 200,
+        "body": values[id]
+    }
+
 def getValues(event, context):
     return {
         "statusCode": 200,
-        "body": json.dumps(["v1", "v2", "v3"])
+        "body": json.dumps(values)
     }
